1861a3a2674245c3f9699c3bdaed706e69b4a17c
morsko1 <morsko1@yandex.ru>
2019-02-21 01:48:57 +0300

    Updates

---
+++ precache-manifest.cf2aed6fba81eb894658bc6eadbf0e33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0bca0fdb62dc33cc7af",
+    "url": "vendors.7aaa0173f82f7280dbc7.js"
+  },
+  {
+    "revision": "2fd37c200e64a8b50e4908614ddec5e8",
+    "url": "sw.js"
+  },
+  {
+    "revision": "c92ff0dddf74a48fa88f47e3ec418a8f",
+    "url": "index.html"
+  },
+  {
+    "revision": "ff61ba170bc26f7c73b26c085a4e3bae",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "b937249cc534b99638e2d526cf821e26",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "cd5de36a5d52556b4fe1",
+    "url": "game.57f891319cb5ced2f6c1.js"
+  },
+  {
+    "revision": "8ea3cd4b088618c4cb6ed4b531b762ab",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "759eb80fa4971db361e3969742863c4a",
+    "url": "assets/img/player.png"
+  },
+  {
+    "revision": "c5abed702d7fbc38759ae10e26d1426c",
+    "url": "assets/img/brick1.jpg"
+  },
+  {
+    "revision": "9417cd98bf504ff59428680aab1cb950",
+    "url": "assets/img/bg.jpg"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.cf2aed6fba81eb894658bc6eadbf0e33.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
